4b3fa7499cf9b018be92466e6fcce89d12de3186
Olaoluwa Osuntokun <laolu32@gmail.com>
2014-10-07 21:59:34 -0700

    trivial linear feedback shift register implementation
    * After a very long, yet enjoyable dance with the Rust's compiler :)

--- rando.rs
+++ rando.rs
@@ -1,5 +1,4 @@
 use std::rand::{Rng, SeedableRng};
-use std::num;
 
 static LCG_MULTIPLIER: u32 = 1103515245;
 static LCG_INCREMENT: u32 = 12345;
@@ -40,13 +39,118 @@ impl SeedableRng<u32> for LinearCongruentialPRNG {
     }
 }
 
+struct LinearFeedbackShiftRegisterPRNG {
+    front_ptr: *mut u32,
+    rear_ptr:  *mut u32,
+    end_ptr: *mut u32,
+    iv: Vec<u32>
+}
+
+impl LinearFeedbackShiftRegisterPRNG {
+    fn init_seed_info(seed: &Vec<u32>) -> (*mut u32, *mut u32, *mut u32, Vec<u32>, u32) {
+        let mut iv = seed.clone();
+
+        let (deg, shift_sep) = match std::mem::size_of_val(&iv) {
+            // x ** 7 + x** 3 + 1
+            x if x < 64 => (7u, 3u),
+            // x**15 + x + 1.
+            x if x < 128 => (15u, 1u),
+            // x**31 + x**3 + 1.
+            x if x < 256 => (31u, 3u),
+            // x**63 + x + 1.  
+            _ => (63u, 1u)
+        };
+
+        for i in range(1u, deg + 1u) {
+            let word = (16807 * iv[i-1]) % 2147483647;
+            *iv.get_mut(i) = word as u32;
+        }
+
+        let front_ptr = unsafe{ iv.as_mut_ptr().offset(shift_sep as int) };
+        let rear_ptr = unsafe{ iv.as_mut_ptr().offset(0) };
+        let end_ptr = unsafe{ iv.as_mut_ptr().offset(deg as int) };
+        (front_ptr, rear_ptr, end_ptr, iv, deg as u32)
+    }
+}
+
+impl Rng for LinearFeedbackShiftRegisterPRNG {
+    fn next_u32(&mut self) -> u32 {
+        let (mut front_ptr, mut rear_ptr, end_ptr) = (self.front_ptr, self.rear_ptr, self.end_ptr);
+
+        let next_rand: u32 = unsafe{ ((*front_ptr + *rear_ptr) >> 1) & 0x7fffffff };
+
+        front_ptr = unsafe{ front_ptr.offset(1) };
+        if front_ptr >= end_ptr {
+            front_ptr = self.iv.as_mut_ptr();
+            rear_ptr = unsafe{ rear_ptr.offset(1) };
+        } else {
+            rear_ptr = unsafe{ rear_ptr.offset(1) };
+            if rear_ptr >= end_ptr {
+                rear_ptr = self.iv.as_mut_ptr();
+            }
+        }
+
+        self.front_ptr = front_ptr;
+        self.rear_ptr = rear_ptr;
+
+        next_rand
+    }
+}
+
+impl SeedableRng<Vec<u32>> for LinearFeedbackShiftRegisterPRNG {
+    fn reseed(&mut self, seed: Vec<u32>) {
+        let reseed_info = LinearFeedbackShiftRegisterPRNG::init_seed_info(&seed);
+        self.front_ptr = reseed_info.clone().val0();
+        self.rear_ptr = reseed_info.clone().val1();
+        self.end_ptr = reseed_info.clone().val2();
+        self.iv = reseed_info.clone().val3();
+
+        let deg = reseed_info.clone().val4();
+
+        for _ in range(0, (deg + 1) * 10) {
+            let _ = self.next_u32();
+        }
+    }
+
+    fn from_seed(seed: Vec<u32>) -> LinearFeedbackShiftRegisterPRNG {
+        let (front_ptr, rear_ptr, end_ptr, iv, deg) = LinearFeedbackShiftRegisterPRNG::init_seed_info(&seed);
+
+        let mut rng = LinearFeedbackShiftRegisterPRNG{ 
+            front_ptr: front_ptr, rear_ptr: rear_ptr, end_ptr: end_ptr, iv: iv
+        };
+
+        for _ in range(0, (deg + 1) * 10) {
+            rng.next_u32();
+        }
+
+        rng
+    }
+}
+
+
 enum PRNG {
     LinearCongruentialPRNG,
+    LinearFeedbackShiftRegisterPRNG
 }
 
 fn main() {
-    let mut rng = LinearCongruentialPRNG::new_unseeded();
-    for _ in range(0u, 20) {
+    let mut rng: LinearCongruentialPRNG = SeedableRng::from_seed(5);
+    for _ in range(0u, 10) {
+        println!("Rand num {}", rng.next_u32() % 100);
+    }
+
+    println!("\n");
+
+    rng.reseed(434);
+    for _ in range(0u, 10) {
+        println!("Rand num {}", rng.next_u32() % 100);
+    }
+
+    println!("\n\n");
+
+    let seed: Vec<u32> = range(9, 43).collect();
+    let mut rng: LinearFeedbackShiftRegisterPRNG = SeedableRng::from_seed(seed);
+    for _ in range(0u, 10) {
         println!("Rand num {}", rng.next_u32() % 100);
     }
 }
